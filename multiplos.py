7cdf8874141ad16056d63d06eb190307b1823eb4
EmoPapu13 <futemo1307@gmail.com>
2025-09-15 00:25:36 -0600

    multiplos_primer_intento_Emiliano

--- multiplos.py
+++ multiplos.py
@@ -3,13 +3,20 @@ Inserta el encabezado aquí y escribe tu código abajo
 """
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+numero_1=int(input("Ingrese el primer numero: "))
+numero_2=int(input("Ingresa el segundo numero: "))
 
 # Proceso
-
+if numero_2 != 0:
+    num_final=(numero_1%numero_2==0)
+else:
+    multiplo_no=False
 
 # Salidas
-print(salida)
+if num_final:
+    print(numero_1, "es multiplo de", numero_2)
+else:
+    print("ninguno de los dos numeros son multiplos")
