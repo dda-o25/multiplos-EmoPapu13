08a7d48442b5b955dc833eabe46b00f789fa4664
EmoPapu13 <futemo1307@gmail.com>
2025-09-15 09:15:40 -0600

    segundo_intento_emiliano

--- multiplos.py
+++ multiplos.py
@@ -10,13 +10,16 @@ numero_1=int(input("Ingrese el primer numero: "))
 numero_2=int(input("Ingresa el segundo numero: "))
 
 # Proceso
-if numero_2 != 0:
-    num_final=(numero_1%numero_2==0)
+if numero_2 == 0:
+    multiplo=False
+    mensaje_final="No se puede dividir entre 0 bro"
 else:
-    multiplo_no=False
+    if numero_1 % numero_2 == 0:
+        multiplo=True
+        mensaje_final=numero_1, "si es multiplo de", numero_2
+    else:
+        multiplo=False
+        mensaje_final=numero_1, "no es multiplo de ", numero_2
 
 # Salidas
-if num_final:
-    print(numero_1, "es multiplo de", numero_2)
-else:
-    print("ninguno de los dos numeros son multiplos")
+print(mensaje_final)
